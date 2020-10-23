e062a583c5e48d5b47eb5a4fff76e9a4eafeef0c
chetas11 <cbirmole@gmail.com>
2020-10-23 22:58:49 +0530

    fetched repo

--- script.js
+++ script.js
@@ -4,9 +4,12 @@ let Name = document.getElementById("Name")
 let Repo = document.getElementById("Repo")
 let CreateDate = document.getElementById("Date")
 let ProfilePic = document.getElementById("avatar")
+let MainDiv = document.getElementById("main")
 
 const client_id = "Iv1.26cc0418ae5a8a34";
 const secretKey = '18d851447cc29f79e084a5aa398c4ad861b4645a'
+
+
 
 
 SubmitBtn.addEventListener("click", ()=>{
@@ -22,7 +25,33 @@ SubmitBtn.addEventListener("click", ()=>{
         CreateDate.innerText = "Created Date: "+data.created_at.substring(0,10);
         
         
+        repoURL = data.repos_url;
+        const Allrepos = await fetch(`${repoURL}`)
+        const repoData = await Allrepos.json();
+
+        function RepoList(i){
+            let Repo1 = document.createElement("p");
+            Repo1.innerText = i+1 +". "+repoData[i].name;
+            MainDiv.append(Repo1)
+        }
+
+        for(let i=0; i<repoData.length; i++){
+            RepoList(i)
+        }
+
+
+        console.log(repoData)
+
+        
     }
+
+    // const fetchRepo = async(userrepo) => {
+    //     const Users = await fetch(`https://api.github.com/user/repos?page=2&per_page=100/chetas11?client_id=Iv1.26cc0418ae5a8a34&client_secret=18d851447cc29f79e084a5aa398c4ad861b4645a`)
+    //     const data = await Users.json();
+    // }
+
+    // https://api.github.com/chetas11?client_id=Iv1.26cc0418ae5a8a34&client_secret=18d851447cc29f79e084a5aa398c4ad861b4645a/repos?page=2&per_page=100/
+
 
     fetcher(UserName);
 })
